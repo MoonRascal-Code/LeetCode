c96d5e91cfd749a718c1d432d7fe3185fa2c8cdf
Jaeyun <76769946+Jaeyun-Moon@users.noreply.github.com>
2025-11-10 16:05:55 +0900

    Time: 7 ms (12.63%), Space: 18 MB (27.26%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -1,14 +1,14 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        table = collections.defaultdict(int)
+        # collections.Counter함수로 간단하게 만들 수 있음
+        table = collections.defaultdict(int) 
         for i in s:
             table[i] +=1
 
         stack = [] 
-        
-        for char in s:
+        for char in s: 
             table[char] -=1 
-            if char in stack:
+            if char in stack: # set 자료형을 사용해서도 풀 수 있음 
                 continue
             while stack and stack[-1] > char and table[stack[-1]] !=0:
                 stack.pop()
