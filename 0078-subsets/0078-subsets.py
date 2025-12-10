698c706bf6418cfe60f69122d52ae44913c63a47
Jaeyun <76769946+Jaeyun-Moon@users.noreply.github.com>
2025-12-11 01:20:37 +0900

    Time: 0 ms (100%), Space: 18 MB (20.57%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,12 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        result = [] 
-        def dfs(index,path):
+        res = [] 
 
-            result.append(path)
+        def dfs(index,prev):
+            res.append(prev)
+            if len(prev) == len(nums):
+                return 
+            
 
-            for i in range(index,len(nums)):
-                dfs(i+1,path+[nums[i]])
+            for ind in range(index,len(nums)):
+                dfs(ind+1,prev+[nums[ind]])
 
         dfs(0,[])
-        return result 
+        return res
