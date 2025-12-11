985daab7a2ff80fc59cb303d98c95de75f3cb309
Jaeyun <76769946+Jaeyun-Moon@users.noreply.github.com>
2025-12-11 21:47:06 +0900

    Time: 5 ms (23.4%), Space: 18.3 MB (27.87%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,18 +1,22 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        # 1. 사전 순이기 때문에, sorted(reverse)
+        # 2. 그래프 생성 (dict)
+        # 3. 오일러 법칙 
+        res = [] 
+        # 1,2 
         graph = collections.defaultdict(list)
+        for start,end in sorted(tickets,reverse=True):
+            graph[start].append(end)
 
-        for a,b in sorted(tickets):
-            graph[a].append(b)
+        def dfs(start):
+            while graph[start]:
+                dfs(graph[start].pop())
+            res.append(start)
+        
+        dfs("JFK")
+        return res[::-1]
+        
 
-        print("graph",graph)
-
-        route,stack = [],["JFK"]
-
-        while stack:
-            while graph[stack[-1]]:
-                stack.append(graph[stack[-1]].pop(0))
-                print(stack)
-            route.append(stack.pop())
-
-        return route[::-1]
+        
+        
