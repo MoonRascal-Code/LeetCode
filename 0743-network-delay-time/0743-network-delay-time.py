874e010309812f64be0f8f70bb9afc9201131bda
Jaeyun <76769946+Jaeyun-Moon@users.noreply.github.com>
2025-12-15 09:32:06 +0900

    Time: 418 ms (10.89%), Space: 20.3 MB (8.67%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,25 +1,28 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         # 1. 모든 노드가 신호를 받는데 걸리는 시간 
-        # 1-1 그래프화  { node : ( vertex, dist)}
+        # 그래프화 
         graph = collections.defaultdict(list)
-        for s, v, d in times:
-            graph[s].append((v,d))
-        # 1-2 큐 변수 정의 및 거리 변수 정의 
-        Q = [(0,k)] 
-        # 거리, vertex -> 최소 heaq 모듈을 활용하여, 최소 경로를 구하기 위해 
-        dist = collections.defaultdict(int)
-        # 1-3 큐 순회 
-        while Q: 
-            time, vertex = heapq.heappop(Q)
-            if vertex not in dist:
-                dist[vertex] = time
-                for v,d in graph[vertex]:
-                    alt = time + d
+        for node, vertex, time in times:
+            graph[node].append([vertex,time])
+
+        # 큐 변수 
+        Q = [(0,k)] # time, node 
+        dist = collections.defaultdict(int) # 거리 저장 용 
+
+        # 큐 순회 
+        while Q:
+            time, node =heapq.heappop(Q)
+            if node not in dist:
+                dist[node] = time
+                for v,t in graph[node]:
+                    alt = time + t 
                     heapq.heappush(Q,(alt,v))
-            
-        # 2. 모든 노드에 도달할 수 있는지 여부 
-        if n == len(dist):
-            return max(dist.values())
         
-        return -1
+
+        if len(dist.keys()) == n:
+            return max(dist.values())
+
+        return -1 
+
+        # 2. 모든 노드에 도달할 수 있는지 여부 
