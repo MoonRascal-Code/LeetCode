9c3e2eb7d2f661165bd6773d309ad88cce78f5a9
Jaeyun <76769946+Jaeyun-Moon@users.noreply.github.com>
2025-12-28 02:55:24 +0900

    Time: 7 ms (46.81%), Space: 19.8 MB (99.18%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -8,22 +8,15 @@ class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         self.longest = 0 
         def dfs(node:TreeNode):
-            # nonlocal longest # # ! 함수 내부에서 할당이 있으면 그 이름은 "지역 변수"로 간주된다. 
-            # 종료 조건 및 패널티 
-            if not node: 
-                # ! None 에는 .val이 없기 때문에, node.val하면 오류가 발생 
-                return -1 
+            if node is None:
+                return -1
             
             left = dfs(node.left)
             right = dfs(node.right)
 
-            self.longest = max(self.longest, left+right+2) # 2는 현재 노드에서 좌우 노드 간선을 더해준 값
-            # ! 함수 내부에서 할당이 있으면 그 이름은 "지역 변수"로 간주된다. 
+            self.longest = max(self.longest,left+right+2) # 거리값, 최대 길이 
+            # 왼쪽 부터 오른쪽 reaf노드 까지의 거리 
+            return max(left,right)+1 # 상태 값 
 
-            return max(left,right) +1 
-            # -1 상쇄 목적의 +1 이기도 하면서 
-            # 간선 하나의 길이 
-        dfs(root)
-        return self.longest 
-            
-
+        dfs(root)        
+        return self.longest
