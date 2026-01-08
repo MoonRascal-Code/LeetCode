4de40cb6b1f2f0b50dd286c12c8c0435dd2b75b3
Jaeyun <76769946+Jaeyun-Moon@users.noreply.github.com>
2026-01-08 15:04:02 +0900

    Time: 0 ms (100%), Space: 19.4 MB (15.32%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,37 +1,20 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        # 문제 본질 
-        # 패턴 파악 : 트리형태로 모든 경로를 탐색하는 DFS 
-        # 알고리즘 설계 
-        # 1. 현재 인덱스의 숫자를 하나 가져오고, 
-        # 2. 그 숫자에 대한 문자 리스트를 반복하며, 
-        # 3. 각 문자를 현재 문자열에 추가 
-        # 4. 다음 숫자로 넘어감(재귀)
-        # 5. 문자열 길이가 digits 길이와 같으면 결과에 추가 
-        # 6. 돌아와서 다음 문자를 붙여보며 탐색 
-
         if not digits:
             return [] 
-        
-        phone = {
-            "2": "abc",
-            "3": "def",
-            "4": "ghi",
-            "5": "jkl",
-            "6": "mno",
-            "7": "pqrs",
-            "8": "tuv",
-            "9": "wxyz"
-        }
 
-        def dfs(index,path):
+        dic = {"2" : "abc", "3" :"def", "4":"ghi","5":"jkl","6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
+        result = [] 
+        def dfs(index, path):
+            # 종료 조건 
             if len(path) == len(digits):
                 result.append(path)
                 return 
-
-            for s in phone[digits[index]]:
-                dfs(index+1,path+s)
-        result = [] 
-        dfs(0,'')
-
+            
+            for i in range(index,len(digits)):
+                for j in dic[digits[i]]:
+                    dfs(i + 1, path + j)
+            
+        dfs(0,"")
         return result 
+            
