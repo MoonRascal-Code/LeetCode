10b27cc7942fe50e523383c6b898d10269c0a697
Jaeyun <76769946+Jaeyun-Moon@users.noreply.github.com>
2026-01-10 17:14:42 +0900

    Time: 101 ms (88.63%), Space: 61.6 MB (8.19%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,17 +1,16 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        res = [] 
-        path = [] # 현재 선택한 숫자들 
-
-        def dfs(start):
-            if len(path) == k:
-                res.append(path[:])
+        result = [] 
+        
+        def dfs(elements,ind,k):
+            if k == 0:
+                result.append(elements[:])
                 return 
-
-            for char in range(start,n+1):
-                path.append(char) # 선택 
-                dfs(char+1) # 다음 선택 // 놓친 부분 
-                path.pop() # 백트래킹 // 놓친 부분 
-        dfs(1)
-
-        return res
+            
+            for i in range(ind,n+1):
+                elements.append(i)
+                dfs(elements,i+1,k-1)
+                elements.pop() 
+        
+        dfs([],1,k)
+        return result 
