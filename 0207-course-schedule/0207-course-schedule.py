99465f08f2ced4c347ba751de1315eea9dc549ee
Jaeyun <76769946+Jaeyun-Moon@users.noreply.github.com>
2026-01-13 18:47:20 +0900

    Time: 11 ms (13.48%), Space: 21.7 MB (5.27%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,11 +1,11 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # 1. 그래프화
+        traced = set() 
+        visited = set() 
+
         graph = collections.defaultdict(list)
         for x,y in prerequisites:
             graph[x].append(y)
-        # 2. 방문 여부를 확인 하고, 가지치기를 위한 각각의 set 
-        traced, visited = set(),set()
 
         def dfs(i):
             if i in traced:
@@ -13,14 +13,12 @@ class Solution:
             if i in visited:
                 return True 
 
-            # 3. 접점을 처음 도달 했을 떼(이웃도 아님)
             traced.add(i)
             for y in graph[i]:
                 if not dfs(y):
                     return False 
-            # 4. 탐색 종료 후 순환 노드 삭제 
+                
             traced.remove(i)
-            # 5. 탐색 종료 후 방문 노드 추가(가지치기)
             visited.add(i)
 
             return True 
@@ -28,5 +26,4 @@ class Solution:
         for x in list(graph):
             if not dfs(x):
                 return False 
-
         return True 
