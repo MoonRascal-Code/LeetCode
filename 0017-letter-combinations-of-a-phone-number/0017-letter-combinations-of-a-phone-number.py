230b170e390df72a3bd6f2103ffde59b43f40465
Jaeyun <76769946+Jaeyun-Moon@users.noreply.github.com>
2026-01-23 18:27:20 +0900

    Time: 55 ms (0.06%), Space: 19.4 MB (12.22%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,20 +1,24 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
+        if not digits: # 빼먹은 부분 
             return [] 
 
-        dic = {"2" : "abc", "3" :"def", "4":"ghi","5":"jkl","6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
+        graph = {'2': 'abc', '3':'def', '4':'ghi',
+                    '5':'jkl','6':'mno','7':'pqrs',
+                    '8':'tuv','9':'wxyz'}
+        
         result = [] 
         def dfs(index, path):
-            # 종료 조건 
-            if len(path) == len(digits):
+            if len(digits) == len(path):
                 result.append(path)
                 return 
             
             for i in range(index,len(digits)):
-                for j in dic[digits[i]]:
-                    dfs(i + 1, path + j)
-            
+                for j in graph[digits[i]]:
+                    # dfs(index+1,path+j)
+                    dfs(i+1,path+j)
+        
         dfs(0,"")
         return result 
-            
+
+        
