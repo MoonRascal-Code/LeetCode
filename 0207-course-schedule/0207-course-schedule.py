dcbdbe4982d20a50051426fb89e08255c33da9fe
Jaeyun <76769946+Jaeyun-Moon@users.noreply.github.com>
2026-01-26 17:11:34 +0900

    Time: 8 ms (22.42%), Space: 21.7 MB (5%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,29 +1,32 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # graph 화 
+        graph = collections.defaultdict(list)
+        for s,e in prerequisites:
+            graph[s].append(e)
+        # 순환 구조 판별을 위한 set 
         traced = set() 
         visited = set() 
 
-        graph = collections.defaultdict(list)
-        for x,y in prerequisites:
-            graph[x].append(y)
-
         def dfs(i):
+            # 순환 구조면 False 
             if i in traced:
                 return False 
+
             if i in visited:
                 return True 
-
-            traced.add(i)
+            
+            traced.add(i) # 0 , 1 ,2 / 0, 2  
             for y in graph[i]:
                 if not dfs(y):
                     return False 
-                
             traced.remove(i)
             visited.add(i)
-
             return True 
 
         for x in list(graph):
             if not dfs(x):
-                return False 
+                return False  
+
         return True 
+
