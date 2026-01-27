11bb698d26dbfdf78a5e78a4ea2e0e9775f98901
Jaeyun <76769946+Jaeyun-Moon@users.noreply.github.com>
2026-01-27 17:50:08 +0900

    Time: 42 ms (81.07%), Space: 22.2 MB (28.73%) - LeetHub

--- 0687-longest-univalue-path/0687-longest-univalue-path.py
+++ 0687-longest-univalue-path/0687-longest-univalue-path.py
@@ -6,27 +6,24 @@
 #         self.right = right
 class Solution:
     def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
-        self.result = 0 
-        def dfs(node:TreeNode):
-            if node is None:
+        self.longest = 0
+        def dfs(node):
+            if not node:
                 return 0 
             
-            left = dfs(node.left)
+            left = dfs(node.left) # 길이 
             right = dfs(node.right)
 
-            # 같은 요소의 길이를 구하는 식 
-            if node.left and node.left.val == node.val:
-                left +=1
-            else:
+            # 현재 노드에서 이전 자식 노드 비교 
+            if node.left and node.left.val == node.val: # 크기 비교 
+                left +=1 
+            else :
                 left = 0 
-            
             if node.right and node.right.val == node.val:
                 right +=1 
-            else: 
-                right =0
-
-            self.result = max(self.result,left+right)
-            return max(left,right) # 큰 쪽으로만 가면 됨
+            else:
+                right =0 
+            self.longest = max(self.longest, left+right)
+            return max(left,right)
         dfs(root)
-        return self.result 
-        
+        return self.longest
