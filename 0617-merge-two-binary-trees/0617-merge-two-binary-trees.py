3f1bc99980df9663a1e71d48f44b620f43cdab60
Jaeyun <76769946+Jaeyun-Moon@users.noreply.github.com>
2026-01-29 00:14:20 +0900

    Time: 0 ms (100%), Space: 19.6 MB (29.6%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
@@ -6,29 +6,17 @@
 #         self.right = right
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-        if root1 is None:
-            return root2 
-        if root2 is None:
-            return root1 
         
-        que = collections.deque()
-        que.append((root1,root2))
+        def dfs(t1 , t2):
+            if t1 and t2:
+                t1.val+= t2.val 
 
-        while que:
-            # print(que.popleft()
-            n1,n2 = que.popleft()
+                t1.left= dfs(t1.left, t2.left)
+                t1.right = dfs(t1.right, t2.right)
 
-            n1.val += n2.val
+            else:
+                if t2:
+                    t1 = t2
+            return t1
 
-            if n1.left and n2.left:
-                que.append((n1.left,n2.left))
-            elif not n1.left:
-                n1.left = n2.left
-                # n1 을 반환할꺼기 때문에, n2없는 경우는 신경안써도 된다. 
-
-            if n1.right and n2.right:
-                que.append((n1.right,n2.right))
-            elif not n1.right:
-                n1.right = n2.right 
-            
-        return root1
+        return dfs(root1, root2)
