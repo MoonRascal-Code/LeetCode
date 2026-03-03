97be385b2a1af64757fad57cde703e8c6c9bf37d
Jaeyun <76769946+MoonRascal-Code@users.noreply.github.com>
2026-03-03 19:12:01 +0900

    Time: 0 ms (100%), Space: 20.6 MB (33.92%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,6 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        ind = bisect.bisect_left(nums,target)
+        if ind < len(nums) and nums[ind] == target:
+            return ind
+        return -1 
